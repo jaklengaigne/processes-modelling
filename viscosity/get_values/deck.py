3783a03eea7094a606b47dacb3286c0e4f7642df
jaklengaigne <67014404+jaklengaigne@users.noreply.github.com>
2020-08-03 23:46:35 -0400

    pep 8 - double lines, single import per line and trailing space

--- viscosity/get_values/deck.py
+++ viscosity/get_values/deck.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import yaml, sys
+import yaml
+import sys
 import os.path
 
-class Deck(): 
+
+class Deck():
 
     def __init__(self, inputhpath):
-        
+
         if not os.path.exists(inputhpath):
             print("File " + inputhpath)
             sys.exit(1)
         else:
-            with open(inputhpath,'r') as f:
-                ## Container of the tags parsed from the yaml file
+            with open(inputhpath, 'r') as f:
+                # Container of the tags parsed from the yaml file
                 self.doc = yaml.load(f, Loader=yaml.BaseLoader)
-                
