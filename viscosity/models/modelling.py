5817f73fb4099960b5bdd145e4fe114516615ac6
jaklengaigne <67014404+jaklengaigne@users.noreply.github.com>
2020-08-03 23:44:53 -0400

    pep8 - trailing space and line length

--- viscosity/models/modelling.py
+++ viscosity/models/modelling.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
-Reference : 
-Mark G. Dodin Ph.D. (1986) 
+Reference :
+Mark G. Dodin Ph.D. (1986)
 Mathematical Models of Polymer Melt Viscosity in Shearing Flow.
 Polystyrene and Polypropylene Melts—2
 International Journal of Polymeric Materials and Polymeric Biomaterials
@@ -22,16 +22,17 @@ class Model:
         self.polymer = polymer
 
     def viscosity(self, B, E, R, T, b, shear_stress_power):
-        """ 
+        """
         valid for all polymers
-        
-        :Input:  
+
+        :Input:
         - *B, b* : constant of the material
-        - *E* : activation energy of viscous-elastic flow under condition of shear stress = constant
+        - *E* : activation energy of viscous-elastic flow under
+                condition of shear stress = constant
         - *shear_stress_power* : shear stresse to the power of 1/2
         - *R* : gas constant in J/(mol.K)
         - *T* : temperature of experiment (K)
-        
+
         :Returns:
         Melt viscosity in shearing flow
 
